ecc93358cccfd53c82ea1d132b32bfd9bb7e98f0
devmannic <82296715+devmannic@users.noreply.github.com>
2021-12-04 16:16:45 -0500

    add proposed blueprint static linkage example with scrypto diff

---
+++ hello-crosscall-static/tests/lib.rs
@@ -0,0 +1,40 @@
+use radix_engine::ledger::*;
+use radix_engine::transaction::*;
+use scrypto::prelude::*;
+
+#[test]
+fn test_hello() {
+    // Set up environment.
+    let mut ledger = InMemoryLedger::with_bootstrap();
+    let mut executor = TransactionExecutor::new(&mut ledger, 0, 0);
+    let key = executor.new_public_key();
+    let account = executor.new_account(key);
+    let package = executor.publish_package(include_code!());
+
+    // Test the `new` function.
+    let transaction1 = TransactionBuilder::new(&executor)
+        .call_function(package, "Hello", "new", vec![], None)
+        .build(vec![key])
+        .unwrap();
+    let receipt1 = executor.run(transaction1, false).unwrap();
+    println!("{:?}\n", receipt1);
+    assert!(receipt1.success);
+
+    // Test the `free_token` method.
+    //let component = receipt1.component(1).unwrap(); // the secondary one is instantiated first, so this is the first one
+    //let component2 = receipt1.component(0).unwrap(); // this still has a public address but not sure if the `_main` is routed properly
+    let component = receipt1.component(0).unwrap(); // only one component is instantiated in this -static version
+    let transaction2 = TransactionBuilder::new(&executor)
+        .call_method(component, "free_token", vec![], Some(account))
+        .drop_all_bucket_refs()
+        .deposit_all_buckets(account)
+        .build(vec![key])
+        .unwrap();
+    let receipt2 = executor.run(transaction2, false).unwrap();
+    println!("{:?}\n", receipt2);
+    assert!(receipt2.success);
+
+    // can't go direct to component 2 because it doesn't exist (and also visibility is pub(crate)
+    let is_none_component = receipt1.component(1).is_none();
+    assert!(is_none_component);
+}
